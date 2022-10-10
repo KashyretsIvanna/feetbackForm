a53368ace430442eb7afb6c7f5ef82d504289b73
Kashyrets Ivanna <kasirecivanna@gmail.com>
2022-10-10 23:29:30 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 8080);
-  var cors = require('cors');
 
-  app.use(cors());
+  app.enableCors();
   console.log('server running', process.env.PORT || 8080);
 }
 bootstrap();
